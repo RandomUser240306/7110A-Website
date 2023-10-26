c79265816a8dc04d84e89fa13b195d478b7690af
Ashwin-Iyer1 <72718544+Ashwin-Iyer1@users.noreply.github.com>
2023-10-26 09:18:01 -0500

    change favicon stuff

--- 7110a-website/src/Home.js
+++ 7110a-website/src/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
-import "./Home.css"
+import "./Home.css" 
+import {Canvas} from "@react-three/fiber";
+import {useGLTF, Stage, PresentationControls} from "@react-three/drei"; 
 
-export default function 
+function Model(props){
+  const {scene} = useGLTF("https://raw.githubusercontent.com/Ashwin-Iyer1/7110A-Website/raw/main/7110a-website/src/Assembly_1.gltf");
+  return <primitive object={scene} {...props} /> 
+}
+export default function Home
 () {
   return (
     <div>
+      <Canvas 
+          dpr={[1,2]} 
+          shadows camera={{fav: 45}}
+          style={{"position": "absolute"}}
+        >
+          <PresentationControls 
+            speed={1.5} 
+            global zoom={0.5}
+            polar={[-0.1, Math.PI / 4]}
+          >
+            <Stage environment={null}>
+              <Model scale={0.01} />
+            </Stage>
+          </PresentationControls>
+        </Canvas>
         <div className="content">
             <h1>Carroll <span>Robotics</span></h1>
             <h1 className="portfolio-text">7110A Website</h1>
