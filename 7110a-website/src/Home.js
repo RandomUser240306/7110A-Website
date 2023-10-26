942b2281d22ee0392558004c05d405b2dc5edf41
Ashwin-Iyer1 <72718544+Ashwin-Iyer1@users.noreply.github.com>
2023-10-26 13:00:55 -0500

    some modelstuff

--- 7110a-website/src/Home.js
+++ 7110a-website/src/Home.js
@@ -1,38 +1,39 @@
-import React from 'react'
-
-import "./Home.css" 
-import {Canvas} from "@react-three/fiber";
-import {useGLTF, Stage, PresentationControls} from "@react-three/drei"; 
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, OrbitControls, Stage } from '@react-three/drei';
 import { MeshoptDecoder } from '@meshopt/meshopt';
+import './Home.css';
 
-function Model(props){
-  const {scene} = useGLTF("https://media.githubusercontent.com/media/Ashwin-Iyer1/7110A-Website/main/bot.glb", MeshoptDecoder);
-  return <primitive object={scene} {...props} /> 
+function Model(props) {
+  const { scene } = useGLTF('https://media.githubusercontent.com/media/Ashwin-Iyer1/7110A-Website/main/bot.glb', MeshoptDecoder);
+  return <primitive object={scene} rotation={[Math.PI / -2, 0, 0]} {...props} />;
 }
 
-export default function Home
-() {
+export default function Home() {
   return (
     <div>
-      <Canvas 
-          dpr={[1,2]} 
-          shadows camera={{fav: 45}}
-          style={{"position": "absolute"}}
+      <div className="content">
+        <h1>
+          Carroll <span>Robotics</span>
+        </h1>
+        <h1 className="portfolio-text">7110A Website</h1>
+      </div>
+      <div className='Model-container'>
+      <Canvas shadows gl={{ antialias: true }} dpr={[1, 1.5]} camera={{ position: [4, 0, 15], fov: 60 }} className='Model'>
+        <color attach="background" args={['skyblue']} />
+        <Stage
+          intensity={0.5}
+          preset="rembrandt"
+          shadows={{ type: 'accumulative', color: 'skyblue', colorBlend: 2, opacity: 1 }}
+          adjustCamera={1}
+          environment="city"
         >
-          <PresentationControls 
-            speed={1.5} 
-            global zoom={0.5}
-            polar={[-0.1, Math.PI / 4]}
-          >
-            <Stage environment={null}>
-              <Model scale={.1} />
-            </Stage>
-          </PresentationControls>
-        </Canvas>
-        <div className="content">
-            <h1>Carroll <span>Robotics</span></h1>
-            <h1 className="portfolio-text">7110A Website</h1>
-        </div>
+          <Model scale={1}/>
+        </Stage>
+        <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.9} makeDefault />
+      </Canvas>
+
+      </div>
     </div>
-  )
+  );
 }
