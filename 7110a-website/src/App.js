8bbc1ab7b45d35a2d92d43e60ab1a3f92ed8b60c
Ashwin-Iyer <ashwiniyer06@gmail.com>
2023-11-07 09:30:14 -0600

    I hate this

--- 7110a-website/src/App.js
+++ 7110a-website/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { createBrowserHistory } from 'history';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { HashRouter as Router, Route, Routes   } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
@@ -9,19 +7,18 @@ import Members from "./Members";
 import Cads from "./cads";
 import CanvasComp from "./CanvasComp";
 
-const hist = createBrowserHistory();
 export default function App() {
-  return (
-    <Router history={hist}>
-      <div className="App Index">
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/members" exact element={<Members />} />
-          <Route path="/cads/*" element={<Cads />} />
-          <Route path="/cads/:modelName" element={<CanvasComp />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App Index">
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/members" exact element={<Members />} />
+                    <Route path="/cads/*" element={<Cads />} />
+                    <Route path="/cads/:modelName" element={<CanvasComp />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
